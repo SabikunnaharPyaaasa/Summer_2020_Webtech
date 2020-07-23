8441b2b9834c59de5f6dd2b79c959643fd66df44
SabikunnaharPyaasa <55408189+SabikunnaharPyaasa@users.noreply.github.com>
2020-07-23 16:37:49 +0600

    Email Modified

--- Lab_Task_03/Email.php
+++ Lab_Task_03/Email.php
@@ -3,21 +3,20 @@ if(isset($_POST['submit'])){
  $email=$_POST['email'];
    $pos1 =strpos($email, '@');
    $pos2 = strpos($email, ".com");
-   if(empty($email))
+   if(!empty($email))
    {
-    echo "Email canot be empty";
-   }
-   if($pos1==False && $pos2==False && $pos2>$pos1)
-   {
-    echo "Must be a valid email address (i.e anything@example.com) ";
+    if($pos1==True && $pos2==True && $pos2>$pos1)
+    {
+       echo "Thanks", $email;
+    }
     
-   }
    else
    {
-     echo " Email: ";
-     echo $email;
+     echo " Invalid ";
+     
    }
-  }
+ }
+ }
 ?>
 
 <!DOCTYPE html>
@@ -27,7 +26,7 @@ if(isset($_POST['submit'])){
 
       <title></title>
       <form method="POST">
-        Email:<input type="email" name = "email" value="">
+        Email:<input type="email" name = "email" value="" placeholder="any@example.com">
         <input type="submit" name ="submit" value="Submit">
  </body>
 </html>
