ca8369798700c75603b206954d90f9292a686781
SabikunnaharPyaasa <55408189+SabikunnaharPyaasa@users.noreply.github.com>
2020-08-24 19:34:57 +0600

    Complete Name validation

--- Final_Lab_Task_02/Name/script.js
+++ Final_Lab_Task_02/Name/script.js
@@ -1,6 +1,52 @@
-function validate(){
+"use strict"
+var valid=false;
+
+function NameValidity(){
+var  Name= document.getElementById("Name").value;
+if(Name=="")
+{
+	document.getElementById("nameMsg").innerHTML = "! Cannot be empty";
+	window.valid = false;
+	
+}
+else if(Name.length<2 && (Name>='A' && Name<='Z' || Name>='a' && Name<='z'))
+{
+	document.getElementById("nameMsg").innerHTML = "! Contains at least two words";
+	window.valid = false;
+}
+else if(Name>='0' && Name<='9')
+{
+	document.getElementById("nameMsg").innerHTML = "!Must be starts with letter";
+	window.valid = false;
+}
+
+else if(Name >='0'  && Name<='9' || Name=='~' || Name=='!' || Name=='@' || Name =='#' || Name=='$' || Name=='%' || Name=='^' || Name=='&' || Name=='*' || Name=='()'|| Name=='/' )
+{
+	document.getElementById('nameMsg').innerHTML = "! Can contain A-Z  a-z or dot(.) or dash(-)";
+	window.valid = false;
+}
+ else if( Name>='A' && Name<='Z' || Name>='a'&& Name<='z' || Name=='.' || Name=='-')
+    {
+        
+                window.valid =true;
+    } 
+}
 
 
+function validate()
+{
+	NameValidity();
+	if(window.valid==true)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
-	return true;
+function Remove()
+{
+	document.getElementById('nameMsg').innerHTML = "";
 }
