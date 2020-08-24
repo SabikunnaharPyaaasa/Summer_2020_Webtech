2104f4857ba3541b407c1a911b2249c2c14736d7
SabikunnaharPyaasa <55408189+SabikunnaharPyaasa@users.noreply.github.com>
2020-08-24 21:02:38 +0600

    Name validation complete

--- Final_Lab_Task_02/Name/namecheck.php
+++ Final_Lab_Task_02/Name/namecheck.php
@@ -1,3 +1,42 @@
 <?php
-echo "";
+if(isset($_POST['Submit']))
+{
+	$name=$_POST['Name'];
+	$valid=FALSE;
+	{
+		if($name!="")
+		{
+			if(strlen($name)>=2)
+			{
+				if($name[0]>='A' && $name[0]<='Z' || $name[0]>='a' && $name[0]<='z')
+				{
+					for($i = 0; $i<strlen($name); $i++)
+					{
+						if(($name[$i]>='A' && $name[$i]<='Z') || ($name[$i]>='a' && $name[$i]<='z') || $name[$i]!='.' || $name[$i]='-'|| $name[$i]!='')
+						{
+							$valid=TRUE;
+						}
+						else
+						{
+							$valid=FALSE;
+						}
+					}
+				}					
+			}
+			else echo "Must be contain at least two word";
+		}
+		else
+		{
+			echo "Invalid user Input for empty";
+		}
+		if($valid==TRUE)
+		{
+			echo $name;
+		}
+		else
+		{
+			echo "Invalid";
+		}
+	}
+}
 ?>
